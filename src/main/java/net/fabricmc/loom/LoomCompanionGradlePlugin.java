d300e67d303edd170db6600b1c18e513087409db
Jonas Herzig <jonas@spark-squared.com>
2026-01-12 11:04:00 +0100

    Fix plugin id of new `loom-companion` plugin

--- src/main/java/net/fabricmc/loom/LoomCompanionGradlePlugin.java
+++ src/main/java/net/fabricmc/loom/LoomCompanionGradlePlugin.java
@@ -33,7 +33,8 @@ import net.fabricmc.loom.task.launch.ExportClasspathTask;
 import net.fabricmc.loom.util.Constants;
 
 public class LoomCompanionGradlePlugin implements Plugin<Project> {
-	public static final String NAME = "dev.architectury.loom-companion";
+	public static final String NAME = "gg.essential.loom-companion";
+	public static final String ARCH_NAME = "dev.architectury.loom-companion";
 	public static final String UPSTREAM_NAME = "net.fabricmc.fabric-loom-companion";
 
 	@Override
--- src/main/java/net/fabricmc/loom/util/gradle/GradleUtils.java
+++ src/main/java/net/fabricmc/loom/util/gradle/GradleUtils.java
@@ -65,7 +65,7 @@ public final class GradleUtils {
 	}
 
 	public static boolean isLoomCompanionProject(Project project) {
-		return project.getPluginManager().hasPlugin(LoomCompanionGradlePlugin.NAME) || project.getPluginManager().hasPlugin(LoomCompanionGradlePlugin.UPSTREAM_NAME);
+		return project.getPluginManager().hasPlugin(LoomCompanionGradlePlugin.NAME) || project.getPluginManager().hasPlugin(LoomCompanionGradlePlugin.ARCH_NAME) || project.getPluginManager().hasPlugin(LoomCompanionGradlePlugin.UPSTREAM_NAME);
 	}
 
 	public static Provider<Boolean> getBooleanPropertyProvider(Project project, String key) {
